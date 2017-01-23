1a3e8e49454ea94fc6ceeccef7e64193f808bdb1
Bestian Tang <besitan@gmail.com>
2017-01-23 11:08:18 +0800

    修改標題和部份樣式

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,8 +21,8 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Page One', component: Page1 },
-      { title: 'Page Two', component: Page2 }
+      { title: '你不必上學', component: Page1 },
+      { title: '自學方法', component: Page2 }
     ];
 
   }
--- src/pages/page2/page2.ts
+++ src/pages/page2/page2.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-
 import { NavController, NavParams } from 'ionic-angular';
+
+
 
 @Component({
   selector: 'page-page2',
@@ -9,29 +10,44 @@ import { NavController, NavParams } from 'ionic-angular';
 export class Page2 {
   selectedItem: any;
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  colors: string[];
+  items: Array<{title: string, note: string, color: string, icon: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
     // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
+    this.icons = ['flask', 
+                  'football', 
+                  'basketball', 
+                  'paper-plane',
+                  'build'];
+
+    this.colors = ['primary', 
+                   'secondary', 
+                  // 'danger', 
+                  // 'light',
+                   'dark'];
+
 
     this.items = [];
     for (let i = 1; i < 11; i++) {
       this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
+        title: '方法 ' + i,
+        note: '不上學，合法嗎?',
+        color: this.colors[i % 3],
+        icon: this.icons[i % 3]
       });
     }
+
+
   }
 
   itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(Page2, {
+    // That's not right, we're pushing to ourselves!
+    this.navCtrl.push(Page2, {      // <-- this shold change to page3(page_detail)
       item: item
     });
   }
