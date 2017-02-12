38fc3135c7f7810d08e2db74512589741cb5b0d5
Bestian Tang 唐宗浩 <besitan@gmail.com>
2017-02-12 18:25:09 +0800

    make page1 UX faster

--- src/pages/page1/page1.ts
+++ src/pages/page1/page1.ts
@@ -16,9 +16,11 @@ export class Page1 {
   nextDate1: number;
   nextDate2: number;
   y: number;
+  openSheet: boolean;
 
   constructor(public actionsheetCtrl: ActionSheetController,public navCtrl: NavController) {
-  	this.y = new Date().getFullYear();
+  	this.openSheet = false;
+    this.y = new Date().getFullYear();
   	this.nextDate0 = new Date('April 30, '+(this.y)+' 00:00:00').getTime();
   	this.nextDate1 = new Date('October 30, '+(this.y)+' 00:00:00').getTime();
   	this.nextDate2 = new Date('April 30, '+(this.y+1)+' 00:00:00').getTime();
@@ -30,9 +32,12 @@ export class Page1 {
 	]
 
   	this.date = Math.floor(Math.min(ds[0],ds[1],ds[2]));
-
   }
 
+  panEvent() {
+    console.log('pan');
+    if (!this.openSheet) this.jumpActionSheet();
+  }
 
   // gotoFabs(e) {
   //   this.navCtrl.push(FabsPage, {})
@@ -40,6 +45,7 @@ export class Page1 {
 
   // jumpActionSheet() 方法是比驚奇箱更快的導航方案
   jumpActionSheet() {
+    this.openSheet = true;
     let actionSheet = this.actionsheetCtrl.create({
      title: '前往',
      buttons: [
@@ -48,6 +54,7 @@ export class Page1 {
          role: 'destructive',
          handler: () => {
            console.log('Destructive clicked');
+           this.openSheet = false;
            this.navCtrl.push(Page2,{})
          }
        },
@@ -55,6 +62,7 @@ export class Page1 {
          text: '時間精靈',
          handler: () => {
            console.log('Time clicked');
+           this.openSheet = false;
            this.navCtrl.push(CalcTimePage,{})
          }
        },
@@ -62,6 +70,7 @@ export class Page1 {
          text: '驚奇箱',
          handler: () => {
            console.log('Fabs clicked');
+           this.openSheet = false;
            this.navCtrl.push(FabsPage,{})
          }
        },
@@ -70,7 +79,8 @@ export class Page1 {
          role: 'cancel',
          handler: () => {
            console.log('Cancel clicked');
-           // 
+           this.openSheet = false;
+            
          }
        }
      ]
